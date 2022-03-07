21af54c7c457e7f9694699452a8f1fe60056936a
Juliana Ifionu <julieifionu@gmail.com>
2022-03-07 12:31:46 +0100

    Create resource for books category actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :categories, only: %i[index create destroy]
+      resources :books, only: %i[index show create update destroy]
     end
   end
 end
