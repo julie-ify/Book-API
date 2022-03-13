1f1ff3af20ba1864e6fad8ccb162749f30c07cd0
Juliana Ifionu <julieifionu@gmail.com>
2022-03-13 12:44:30 +0100

    Define the create action of authentication controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,2 +1,34 @@
-class AuthenticationController < ApplicationController
+module Api
+  module V1
+    class AuthenticationController < ApplicationController
+      class AuthenticateError < StandardError; end;
+
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      rescue_from AuthenticateError, with: :handle_unauthenticated
+
+      def create
+        if user
+          raise AuthenticateError unless user.authenticate(params.require(:pasword))
+          render json: UserReprensenter.new(user).as_json, status: :created
+        else
+          render json: { error: 'No such user' }, status: :unauthorized
+        end
+      end
+
+      private
+      def user
+        @user ||= User.find_by(username: params.require(:username))
+      end
+
+      def parameter_missing(error)
+        render json: { error: error.message }, status: unprocessable_entity
+      end
+
+      def handle_unauthenticated
+        render json: { error: 'Incorrect password' }, status: unauthorized
+      end
+    end
+  end
 end
+    
+
