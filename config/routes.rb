2304559cd61e3a0b3c9179a72c4f19f3422e6305
Juliana Ifionu <julieifionu@gmail.com>
2022-03-13 10:18:49 +0100

    Add login and register routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :categories, only: %i[index create destroy]
       resources :books, only: %i[index show create update destroy]
+      post 'login', to: 'authentication#create'
+      post 'register', to: 'users#create'
     end
   end
 end
